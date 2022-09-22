93310aeda565c6034dc2dc90b237cf1cb3ca8bd1
Crystal Aguilar <crystal_0688@yahoo.com>
2022-09-22 13:52:15 -0700

    adding api, has bugs

--- script.js
+++ script.js
@@ -8,7 +8,23 @@ const currentTemps = document.getElementById("currenttemp");
 
 var APIKey = '1567899baf64751e46a6d93ae8fa5cd8';
 var city;
-var state;
-var queryUrl = 'api.openweathermap.org/data/1.0/forecast?q=' + city + state + '&limit=3' + '&appid=' + APIKey;
+var queryUrl = 'api.openweathermap.org/data/1.0/forecast?q=${city}&limit=3&appid=${APIKey}';
+fetch(queryUrl)
 
-//fetch(queryUrl)
+setInterval(() => {
+    time = moment().format('LTS').innerHTML;
+
+    date = moment().format('ll').innerHTML;
+
+}, 1000)
+
+getWeatherData()
+
+function getWeatherData() {
+    navigator.geolocation.getCurrentPosition((success) => {
+
+        let city = success.coords;
+
+        fetch(queryUrl)
+    })
+}
