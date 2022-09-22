53f13b1039ca5273b5945df5428cd927cc778bcb
Crystal Aguilar <crystal_0688@yahoo.com>
2022-09-22 13:26:20 -0700

    some more csds and starting JS

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const time = document.getElementById('time');
+const date = document.getElementById('date');
+const currenWeather = document.getElementById('currentweather-items');
+const timeZone = document.getElementById("time-zone");
+const country = document.getElementById('country');
+const weatherForecast = document.getElementById("weatherforecast")
+const currentTemps = document.getElementById("currenttemp");
+
 var APIKey = '1567899baf64751e46a6d93ae8fa5cd8';
 var city;
 var state;
