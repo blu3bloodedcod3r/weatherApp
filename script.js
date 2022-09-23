34f048c35b4d6d06e0a716aa804b992c394a01c4
Crystal Aguilar <crystal_0688@yahoo.com>
2022-09-23 15:17:00 -0700

    fixed date to appear

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const time = document.getElementById('time');
 const date = document.getElementById('date');
+button = document.querySelector('button');
 const currenWeather = document.getElementById('currentweather-items');
-const timeZone = document.getElementById("time-zone");
-const country = document.getElementById('country');
 const weatherForecast = document.getElementById("weatherforecast")
 const currentTemps = document.getElementById("currenttemp");
 const city = document.getElementById('city');
@@ -11,26 +10,22 @@ var APIKey = '1567899baf64751e46a6d93ae8fa5cd8';
 
 setInterval(() => {
     var localTime = moment().format("hh:mm");
+    var currentDate = moment().format('MMM-DD-YY');
 
     time.innerHTML = localTime + '<span id="am-pm">PM</span>';
-    date.innerHTML = moment().format('mmm-dd-yyyy');
+    date.innerHTML = currentDate + '<span id="date"></span>'
 
 }, 1000)
 
-getWeatherData()
-
-function getWeatherData(data) {
-    navigator.geolocation.getCurrentPosition((success) => {
-    
-        let {latitude, longitude} = success.corrds;
-
-        fetch('api.openweathermap.org/data/1.0/forecast?lat=${latitude}&lon=${longitude}&q=${city}&limit=37unit=imperial&appid=${APIKey}')
-            .then(response =>response.json())
-            .then(data => {
-                console.log(data)
-            })
-        .catch(err => alert('wrong city name'))
-
-
+button.addEventListener('click', function() {
+    fetch('api.openweathermap.org/data/1.0/forecast?&q='+city.value+'&limit=5&unit=imperial&appid=${APIKey}')
+    .then(response =>response.json())
+    .then(data => {
+        weatherForecast = data['humid']
+        weatherForecast = data['press']
+        weatherForecast = data['windspeed']
     })
-}
+
+    .catch(err => alert('wrong city name'))
+})
+
