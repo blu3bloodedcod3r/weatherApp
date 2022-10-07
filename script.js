fe3693ea0c5bda8a4b6ff1e7803a059a4d16e6be
Crystal Aguilar <crystal_0688@yahoo.com>
2022-10-07 13:52:53 -0700

    fixing weather data and inserted modual

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const time = document.getElementById('time');
 const date = document.getElementById('date');
 const button = document.querySelector('button');
-const currenWeather = document.getElementById('currentweather-items');
+const currentWeather = document.getElementById('currentweatheritems');
 const weatherForecast = document.getElementById("weatherforecast")
 const currentTemps = document.getElementById("currenttemp");
 const city = document.getElementById('city');
+const humid = document.querySelector("#humid");
+const press = document.querySelector('#press');
+const windspeed = document.querySelector('#windspeed');
+const dayTemp1 = document.querySelector("#dayTemp1")
+const searchedCities = document.querySelector("#city");
 
 var APIKey = '1567899baf64751e46a6d93ae8fa5cd8';
 
@@ -15,41 +20,68 @@ setInterval(() => {
     time.innerHTML = localTime
     date.innerHTML = currentDate + '<span id="date"></span>'
 
-}, 1000)
+}, 1000);
 
-button.addEventListener('click', function(e) {
-    e.preventDefault();
-    fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + city.value  + '&limit=5&appid=1567899baf64751e46a6d93ae8fa5cd8')
+runWeather();
+
+//created to be saved for search history
+function init () {
+    searchedCities = JSON.parse(localStorage.getItem("searchedCities")) || [];
+    //createHistory();
+};
+
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+});
+
+//both API's from openweathermap
+function runWeather () {
+
+    //if statement to activate modal when city is empty
+    if (city === ' ') {
+        $('noCityModal#').modal('show')
+           return;
+      } ;
+
+    fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + city  + '&limit=5&appid=1567899baf64751e46a6d93ae8fa5cd8')
     .then(response =>response.json())
     .then(data => {
         var long = data[0].lon
         var lat = data[0].lat
-        // console.log(data)
-        // console.log(data[0].lat)
-        // console.log(data[0].lon)
-    
+        console.log(long)
+        //console.log(data[0])
+        //console.log(long)
+        //console.log(lat)
+    }) 
+
+
+    console.log(data)
+
     fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon='+ long + '&appid=1567899baf64751e46a6d93ae8fa5cd8')
     .then(response =>response.json())
     .then(data => {
-        
         var humidity= data.main.humidity
         var pressure = data.main.pressure
         var windSpeed = data.wind.speed
+        var dayTemp = data.main[4]
+        var nightTemp = data.main[5]
+        //var todaysTempDay= data.temp.currentDate
+        //var tonightsTemp = data.temp.tonight
+        //console.log(data)
+        //console.log(humidity)
+        //console.log(pressure)
 
-        // console.log(data)
-        // console.log(data.main.humidity)
+        humid.innerHTML= humidity + ' %';
+        press.innerHTML = pressure + ' inHg';
+         windspeed.innerHTML = windSpeed + ' MPH';
+        dayTemp1.innerHTML = dayTemp + 'F'
+        //currentWeather.innerHTML = 
 
-        // console.log(humidity)
-        // console.log(pressure)
-        // console.log(windSpeed)
-        const humid = JSON.parse(humidity)
-        console.log(humidity)
-    })
+        //to be set to local storage for current weather
+        localStorage.setItem('humidity', humid)
+        localStorage.setItem('pressure', press)
+        localStorage.setItem('windspeed', windSpeed)
+    });
+}
 
-    })
-    localStorage.setItem('humidity', humid)
-    localStorage.setItem('pressure', press)
-    localStorage.setItem('windspeed', windSpeed)
-    
-})
 
