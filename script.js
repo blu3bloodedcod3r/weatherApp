0f9174a1cd8cd3556475eaef75642a4b6978bb20
Crystal Aguilar <crystal_0688@yahoo.com>
2022-10-07 17:27:17 -0700

    fixed data variable

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const city = document.getElementById('cityinput');
 const humid = document.querySelector("#humid");
 const press = document.querySelector('#press');
 const windspeed = document.querySelector('#windspeed');
-const dayTemp1 = document.querySelector("#dayTemp1")
 const searchedCities = document.querySelector("#city");
 
 var APIKey = '1567899baf64751e46a6d93ae8fa5cd8';
@@ -22,8 +21,6 @@ setInterval(() => {
 
 }, 500);
 
-//runWeather();
-console.log(city)
 //created to be saved for search history
 function init () {
     searchedCities = JSON.parse(localStorage.getItem("searchedCities")) || [];
@@ -33,11 +30,6 @@ function init () {
 button.addEventListener('click', function(event) {
     event.preventDefault();
     //createHistory();
-    //if statement to activate modal when city is empty
-    if (city === ' ') {
-        $('noCityModal#').modal('show')
-           return;
-      } ;
     runWeather(city);
 });
 
@@ -48,38 +40,66 @@ function runWeather () {
     .then(response =>response.json())
     .then(data => {
   
-        var long = data[0].lon
-        var lat = data[0].lat
+        let lat = data[0].lat;
+        let long = data[0].lon;
         //console.log(data[0])
-        console.log(long)
-        console.log(lat)
-    }) 
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon='+ long + '&appid=1567899baf64751e46a6d93ae8fa5cd8')
-    .then(response =>response.json())
-    .then(data => {
-        var humidity= data.main.humidity
-        var pressure = data.main.pressure
-        var windSpeed = data.wind.speed
-        var dayTemp = data.main[4]
-        var nightTemp = data.main[5]
-        //var todaysTempDay= data.temp.currentDate
-        //var tonightsTemp = data.temp.tonight
-        //console.log(data)
-        //console.log(humidity)
-        //console.log(pressure)
+        fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon='+ long + '&units=imperial' + '&appid=1567899baf64751e46a6d93ae8fa5cd8')
+            .then(response =>response.json())
+            .then(data => {
+            //curent info
+           let humidity = data.list[0].main.humidity
+           let pressure = data.list[0].main.pressure
+           let windSpeed = data.list[0].wind.speed
+           let dayTemp1 = data.list[0].main.temp_max
+           let nightTemp1 = data.list[0].main.temp_min
 
-        humid.innerHTML= humidity + ' %';
-        press.innerHTML = pressure + ' inHg';
-         windspeed.innerHTML = windSpeed + ' MPH';
-        dayTemp1.innerHTML = dayTemp + 'F'
-        //currentWeather.innerHTML = 
+            //futurecast info
+           let dayTemp2 = data.list[1].main.temp_max
+           let nightTemp2 = data.list[1].main.temp_min
+           let dayTemp3 = data.list[2].main.temp_max
+           let nightTemp3 = data.list[2].main.temp_min
+           let dayTemp4 = data.list[3].main.temp_max
+           let nightTemp4 = data.list[3].main.temp_min
+           let dayTemp5 = data.list[4].main.temp_max
+           let nightTemp5 = data.list[4].main.temp_min
+            //console.log(data.list)
 
-        //to be set to local storage for current weather
+        //jQuery to show on current and futurecast
+            $("#humid").html(humidity + ' %');
+            $("#press").html(pressure + ' inHg');
+            $("#windspeed").html(windSpeed + ' MPH');
+            $('#dayTemp1').html(dayTemp1)
+            $('#nightTemp1').html(nightTemp1)
+            $('#dayTemp2').html(dayTemp2)
+            $('#nightTemp2').html(nightTemp2)
+            $('#dayTemp3').html(dayTemp3)
+            $('#nightTemp3').html(nightTemp3)
+            $('#dayTemp4').html(dayTemp4)
+            $('#nightTemp4').html(nightTemp4)
+            $('#dayTemp5').html(dayTemp5)
+            $('#nightTemp5').html(() => {
+                nightTemp5.toggle(show)
+            })
+            console.log(nightTemp2)
+
+            
+         //to be set to local storage for current weather
         localStorage.setItem('humidity', humid)
         localStorage.setItem('pressure', press)
         localStorage.setItem('windspeed', windSpeed)
+        localStorage.setItem('dayTemp1', $(dayTemp1))
+        localStorage.setItem('nightTemp1', $(nightTemp1))
+        
+        });
+
+
+        
     });
-}
-
-
+    
+    //if statement to activate modal when city is empty
+    if (city === ' ') {
+        $('noCityModal#').modal('show')
+           return;
+      } ;
+};
